eb89950c6f275a8df850bcda94768cb403feebe9
prat <banqfirn@gmail.com>
2014-09-12 23:55:20 -0400

    moved boilerplate

--- src/com/pt21/afb/SimpleActivity.scala
+++ src/com/pt21/afb/SimpleActivity.scala
@@ -1,8 +1,41 @@
 package com.pt21.afb
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import com.google.android.glass.media.Sounds
+
 /**
  * Created by prt2121 on 9/12/14.
  */
-class SimpleActivity {
+class SimpleActivity extends BaseGlassActivity {
 
+  var next : Class[_ <: Activity] = null
+  var textView : TextView = null
+
+  override def onCreate(savedInstanceState: Bundle): Unit = {
+    super.onCreate(savedInstanceState)
+    setContentView(new TugView(this, R.layout.activity_simple))
+    textView = findViewById(R.id.tv_simple).asInstanceOf[TextView]
+  }
+
+  def nextActivity(activity: Class[Activity]):Unit =  next = activity
+
+  override def onLongPress(): Boolean = {
+    onTap()
+  }
+
+  override def onTap(): Boolean = {
+    playSound(this, Sounds.TAP)
+    startNextActivity()
+    true
+  }
+
+  def startNextActivity(): Unit = {
+    if(next != null) {
+      startActivity(new Intent(this, next).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+      finish()
+    }
+  }
 }
--- src/com/pt21/afb/SplashScreenActivity.scala
+++ src/com/pt21/afb/SplashScreenActivity.scala
@@ -1,84 +1,15 @@
 package com.pt21.afb
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.view.{ViewGroup, View}
-import android.widget.AdapterView
-import com.google.android.glass.app.Card
-import com.google.android.glass.media.Sounds
-import com.google.android.glass.widget.{CardBuilder, CardScrollAdapter, CardScrollView}
 
 /**
  * Created by prt2121 on 9/11/14.
  */
-class SplashScreenActivity extends BaseGlassActivity {
-
-  private var mScrollView: Option[CardScrollView] = None
-  private var mView: Option[View] = None
+class SplashScreenActivity extends SimpleActivity {
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
-
-    mView = Some(buildView)
-
-    mScrollView = Some(new CardScrollView(this))
-    mScrollView.map { scrollView =>
-      scrollView.setAdapter(new CardScrollAdapter() {
-        override def getCount: Int = 1
-
-        override def getPosition(item: scala.Any): Int =
-          if (item == mView) 0
-          else AdapterView.INVALID_POSITION
-
-        override def getView(position: Int, convertView: View, parent: ViewGroup): View = mView.get
-
-        override def getItem(position: Int): AnyRef = mView
-      })
-
-    }
-
-    setContentView(mScrollView.get)
+    next = classOf[LandingActivity]
   }
 
-  override def onResume(): Unit = {
-    super.onResume()
-    mScrollView.map(_.activate)
-  }
-
-  override def onPause(): Unit = {
-    super.onPause()
-    mScrollView.map(_.deactivate)
-  }
-
-  def buildView: View = {
-    try {
-      val cardBuilderName = "com.google.android.glass.widget.CardBuilder"
-      Class.forName(cardBuilderName)
-      new CardBuilder(this, CardBuilder.Layout.TEXT)
-        .addImage(R.drawable.background)
-        .getView
-    } catch {
-      case e: Exception =>
-        new Card(this)
-          .setImageLayout(Card.ImageLayout.FULL)
-          .addImage(R.drawable.background)
-          .getView
-    }
-  }
-
-  override def onTap(): Boolean = {
-    playSound(this, Sounds.TAP)
-    startLandingActivity()
-    true
-  }
-
-  override def onLongPress(): Boolean = {
-    onTap()
-  }
-
-  def startLandingActivity():Unit = {
-    startActivity(new Intent(this, classOf[LandingActivity]).setFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
-    finish()
-  }
 }
