9067b4b7cbbcd3d9a5f92fe742b1aeb2dcc34593
prat <banqfirn@gmail.com>
2014-09-13 12:17:25 -0400

    finished HighScoreActivity

--- src/com/pt21/afb/HighScoreActivity.scala
+++ src/com/pt21/afb/HighScoreActivity.scala
@@ -2,6 +2,8 @@ package com.pt21.afb
 
 import android.content.Context
 import android.os.Bundle
+import android.text.{SpannableString, Spannable}
+import android.text.style.RelativeSizeSpan
 import android.view.{MotionEvent, ViewGroup, View}
 import android.widget.AdapterView
 import com.google.android.glass.widget.{CardBuilder, CardScrollAdapter, CardScrollView}
@@ -9,57 +11,17 @@ import com.google.android.glass.widget.{CardBuilder, CardScrollAdapter, CardScro
 /**
  * Created by prt2121 on 9/8/14.
  */
-class HighScoreActivity extends BaseGlassActivity {
-
-  private var mScrollView: Option[CardScrollView] = None
-  private var mView: Option[View] = None
+class HighScoreActivity extends SimpleActivity {
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
-
     val sharedPref = getSharedPreferences("AngryFlappyBird", Context.MODE_PRIVATE)
     val highScore = sharedPref.getInt("highScore", 0)
-
-    mView = Some(buildViews(highScore))
-    mScrollView = Some(new CardScrollView(this) {
-      override def dispatchGenericFocusedEvent(event : MotionEvent): Boolean = {
-        super.dispatchGenericFocusedEvent(event)
-        false
-      }
-    })
-    mScrollView.map { scrollView =>
-      scrollView.setAdapter(new CardScrollAdapter() {
-        override def getCount: Int = 1
-
-        override def getPosition(item: scala.Any): Int =
-          if (mView.exists(_.equals(item))) 0
-          else AdapterView.INVALID_POSITION
-
-        override def getView(position: Int, convertView: View, parent: ViewGroup): View = mView.get
-
-        override def getItem(position: Int): AnyRef = mView.get
-      })
-
-    }
-    setContentView(mScrollView.get)
+    val prefix = "Your highest score is\n"
+    val text = new SpannableString(prefix + highScore.toString)
+    text.setSpan(new RelativeSizeSpan(0.8f), 0, prefix.length, 33) //33 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    text.setSpan(new RelativeSizeSpan(2.0f), prefix.length, text.length, 33)
+    textView.setText(text)
   }
-
-  override def onResume(): Unit = {
-    super.onResume()
-    mScrollView.map(_.activate)
-  }
-
-  override def onPause(): Unit = {
-    super.onPause()
-    mScrollView.map(_.deactivate)
-  }
-
-  def buildViews(highScore : Int): View = {
-    new CardBuilder(this, CardBuilder.Layout.TEXT)
-      .addImage(R.drawable.background)
-      .setText("Your highest score is " + highScore.toString + ".")
-      .getView
-  }
-
 
 }
